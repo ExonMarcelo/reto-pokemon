de0e6c411eba3eaaf6e1a5d0ba38f2245b1298fc
Exon Marcelo <emarcelo@suizalab.com>
2025-03-13 22:09:04 -0500

    fix: ajustes navegacion

--- src/components/Views/Detail/index.tsx
+++ src/components/Views/Detail/index.tsx
@@ -1,31 +1,37 @@
+import { Link } from "react-router-dom"
 import useDetail from "./useDetail"
 
 export default function Detail(){
     const {isLoading, pokemonSelected} = useDetail()
     return(
         <div>
+            <Link to="/">Volver</Link>
             {
                 isLoading?
                 <p>Cargando...</p>
                 :
-                <div className="flex gap-8">
+                <div className="flex gap-8 mt-8">
                     <div className="flex gap-2 flex-col items-center justify-center">
                         <img src={pokemonSelected?.avatar} alt={pokemonSelected?.name} className="max-w-[240px] max-h-[240px]" />
                         <p className="text-2xl">{pokemonSelected?.name}</p>
+                        
                     </div>
                     <div>
                         <div>
                             <h4 className="text-xl">Abilities:</h4>
-                            <p>{pokemonSelected?.abilitiesMapped?.join(",")}</p>
+                            <p>{pokemonSelected?.abilitiesMapped?.join(", ")}</p>
                         </div>
-                        <div>
+                        <div className="mt-2">
                             <h4 className="text-xl">types:</h4>
-                            <p>{pokemonSelected?.typesMapped?.join(",")}</p>
+                            <p>{pokemonSelected?.typesMapped?.join(", ")}</p>
                         </div>
                         {/* <div className="mt-2">
                             <h4 className="text-xl">Forms:</h4>
                             <p>{pokemonSelected?.typesMapped?.join(",")}</p>
                         </div> */}
+                        <div className="mt-4">
+                            <button className="rounded-full bg-indigo-700 px-4 py-2 text-sm font-semibold text-white" type="button">Capturar</button>
+                        </div>
                     </div>
                 </div>
             }
